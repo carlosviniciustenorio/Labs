0a87bba38d8f8502593c370bbd2171efe1746544
carlosviniciustenorio <carlosviniciustenorio@gmail.com>
2024-07-18 19:31:44 -0300

    ajustes

--- IntegrationRobotGRPC/Program.cs
+++ IntegrationRobotGRPC/Program.cs
@@ -12,6 +12,7 @@ builder.WebHost.ConfigureKestrel(options =>
     options.ListenAnyIP(7073, listenOptions =>
     {
         listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2;
+        listenOptions.UseHttps();
     });
 });
 
@@ -20,10 +21,7 @@ var app = builder.Build();
 app.MapGrpcService<GreeterService>();
 app.MapGrpcService<IntegrationRobotGRPC.Services.CarAdService>();
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapGrpcReflectionService();
-}
+app.MapGrpcReflectionService();
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
--- IntegrationRobotGRPC/obj/Debug/net8.0/IntegrationRobotGRPC.AssemblyInfo.cs
+++ IntegrationRobotGRPC/obj/Debug/net8.0/IntegrationRobotGRPC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IntegrationRobotGRPC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e8058e7d88e3642d03ecf1c41892b4ca039a7f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d67c9b09a7a693c6aa3e5d0f1a2d41bf2e2458cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("IntegrationRobotGRPC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IntegrationRobotGRPC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
